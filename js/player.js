383e6805d5fcb02d4d85387bd8f0a0a0f000bc11
arandomperson <146173916+arandomperson137@users.noreply.github.com>
2025-01-14 09:33:42 +0000

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,11 +1,14 @@
-class player {
+class Player {
     constructor(game) {
         this.game = game;
-        this.x = 50;
-        this.y = 60;
-        this.width = 200;
-        this.height = 200;
-        this.speedY = 0;
+        this.x = 20;
+        this.y = 0;
+        this.spriteWidth = 200;
+        this.spriteHeight = 200;    
+        this.width;
+        this.height;    
+        this.speedY;
+        this.flapSpeed;
     }
 
     draw() {
@@ -14,18 +17,34 @@ class player {
 
     update() {
         this.y += this.speedY;
-        if(this.y < this.game.hright - this.height)  {
-            this.speedY += this.game.gravity;
 
-        }
-        if(this.y >= this.game.height - this.height)    {
+        if(this.isTouchingBottom())    {
             this.y = this.game.height - this.height;
-            console.log
+        }
+        else    {
+            this.speedY += this.game.gravity;
+        }
+    }
 
-    }
-    }
     resize() {
-        this.width = this.sprite.width * this.game.ratio;
-        this.height = this.spriteheight * this.game.ratio;
+        this.width = this.spriteWidth * this.game.ratio;
+        this.height = this.spriteHeight * this.game.ratio;
+        this.y = this.game.height * 0.5 - this.height * 0.5;
+        this.speedY = -8 * this.game.ratio;
+        this.flapSpeed = 6 * this.game.ratio;
+    }
+
+    isTouchingBottom()  {
+        return this.y >= this.game.height - this.height;
+    }
+
+    isTouchingTop() {
+        return this.y <= 0;
+    }
+
+    flap()  {
+        if(!this.isTouchingTop())    {
+            this.speedY = -this.flapSpeed;
+        }
     }
 }
