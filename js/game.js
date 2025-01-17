a182911206b732fe5e42c0c4065012960a1039e4
arandomperson <146173916+arandomperson137@users.noreply.github.com>
2025-01-17 08:48:05 +0000

    collision detection and win or lose

--- js/game.js
+++ js/game.js
@@ -14,6 +14,9 @@ class Game {
         this.speed;
         this.score;
         this.gameOver;
+        this.timer;
+        this.message1;  //Win or Lose
+        this.message2;  //Win or Lose
 
         this.resize(window.innerWidth, window.innerHeight);
 
@@ -47,6 +50,8 @@ class Game {
         this.ctx.fillStyle = '#5995F2';
         this.ctx.font = '15px Bungee';
         this.ctx.textAlign = 'right';
+        this.ctx.lineWidth = 3;   //Collision
+        this.ctx.strokeStyle = 'white';   //Collision
         this.ratio = this.height / this.baseHeight;
 
         this.gravity = 0.15 * this.ratio;
@@ -60,11 +65,15 @@ class Game {
 
         this.score = 0;
         this.gameOver = false;
+        this.timer = 0;
     }
 
-    render() {
+    render(deltaTime) {
+        console.log(deltaTime);
+        if(!this.gameOver) this.timer += deltaTime;
         this.background.update();
         this.background.draw();
+        this.drawStatusText();
         this.player.update();
         this.player.draw();
         this.obstacles.forEach(obstacle => {
@@ -82,11 +91,46 @@ class Game {
             this.obstacles.push(new Obstacle(this, firstX + i * obstacleSpacing));
         }
     }
-    drawStatusText() {
+
+    formatTimer()   {
+        return (this.timer * 0.001).toFixed(1);
+    }
+
+    drawStatusText()    {
         this.ctx.save();
         this.ctx.fillStyle = '#FFFFFF';
         this.ctx.fillText('Score: ' + this.score, this.width - 10, 30);
+        this.ctx.textAlign = 'left';
+        this.ctx.fillText('Timer: ' + this.formatTimer(), 10, 30);
+        
+        if(this.gameOver) {
+            if(this.player.collided)    {  //Win or Lose
+                this.message1 = "Getting rusty?";  //Win or Lose
+                this.message2 = "Collison time " + this.formatTimer() + " seconds!";  //Win or Lose
+            }  //Win or Lose
+            else    {  //Win or Lose
+                this.message1 = "Nailed it!";  //Win or Lose
+                this.message2 = "Can you do it faster than " + this.formatTimer() + " seconds?";  //Win or Lose
+            }  //Win or Lose
+
+            this.ctx.font = '30px Bungee';
+            this.ctx.textAlign = 'center';
+            this.ctx.fillText(this.message1, this.width * 0.5, this.height * 0.5 - 40);  //Win or Lose
+            this.ctx.font = '15px Bungee';  //Win or Lose
+            this.ctx.fillText(this.message2, this.width * 0.5, this.height * 0.5 - 20);  //Win or Lose
+            this.ctx.fillText("Press 'R' to try again!", this.width * 0.5, this.height * 0.5);  //Win or Lose
+        }
+
         this.ctx.restore();
+    }
+
+    checkCollision(a, b)    {   //Collision
+        const dx = a.collisionX - b.collisionX;   //Collision
+        const dy = a.collisionY - b.collisionY;   //Collision
+        const distance = Math.sqrt(dx * dx + dy * dy);   //Collision
+        //const distance = Math.hypot(dx, dy);   //Collision
+        const sumOfRadii = a.collisionRadius + b.collisionRadius;   //Collision
+        return distance <= sumOfRadii;   //Collision
     }
 }
 
@@ -98,10 +142,13 @@ window.addEventListener('load', function() {
 
     const game = new Game(canvas, ctx);
 
-    function animate() {
+    let lastTime = 0;
+    function animate(timeStamp) {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        game.render();
-        if(!gameOver) requestAnimationFrame(animate);
+        game.render(deltaTime);
+        requestAnimationFrame(animate);
     }
 
     requestAnimationFrame(animate);
