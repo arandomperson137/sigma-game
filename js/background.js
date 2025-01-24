70f739668f28802f6c08f59d752f87a00b239e20
arandomperson <146173916+arandomperson137@users.noreply.github.com>
2025-01-24 13:38:37 +0000

    initial game structure

--- js/background.js
+++ js/background.js
@@ -1,30 +1,30 @@
-class Background {
-    constructor(game) {
+class Background    {
+    constructor(game)   {
         this.game = game;
         this.image = document.getElementById('background');
         this.width = 2400;
         this.height = this.game.baseHeight;
-        this.scaleHeight;
         this.scaleWidth;
+        this.scaleHeight;
         this.x;
     }
-    
-    update() {
-    this.x -= 0.5;
-    if(this.x <= -this.width) this.x = 0;
+
+    update()    {
+        this.x -= this.game.speed; 
+        if(this.x <= -this.scaleWidth) this.x = 0;
     }
 
-    draw() {
-        this.game.ctx.drawImage(this.image, this.x, 0, this.scaleWidth, this.scaleHeight);
-        this.game.ctx.drawImage(this.image, this.x + this.scaleWidth, 0, this.scaleWidth, this.scaleWidth);
-        if (this.game.canvas.width >= this.scaleWidth) {
-        this.game.ctx.drawImage(this.image, this.x + this.scaleWidth * 2, 0, this.scaleWidth, this.scaleHeight);
+    draw()  {
+        this.game.ctx.drawImage(this.image, this.x, 0, this.scaleWidth , this.scaleHeight);
+        this.game.ctx.drawImage(this.image, this.x + this.scaleWidth, 0, this.scaleWidth , this.scaleHeight);
+        if(this.game.canvas.width >= this.scaleWidth)  {
+            this.game.ctx.drawImage(this.image, this.x + this.scaleWidth * 2, 0, this.scaleWidth , this.scaleHeight);
         }
     }
 
-    resize() {
-       this.scaleWidth = this.width * this.game.ratio;
-       this.scaleHeight = this.height * this.game.ratio;
-       this.x = 0;
+    resize()    {
+        this.scaleWidth = this.width * this.game.ratio;
+        this.scaleHeight = this.height * this.game.ratio;
+        this.x = 0;
     }
 }
